852f339101b2a9c9b521bba4a0d66bad37e0fe14
OCunningham04 <oacunningham21@my.trine.edu>
2024-10-03 17:30:32 -0400

    Start to sine wave generator

--- Code/Core/Src/olivia.cpp
+++ Code/Core/Src/olivia.cpp
@@ -5,13 +5,28 @@
  *      Author: townl
  */
 #include "allIncludes.h"
+#include <vector>
+#include <cmath>
 
 //Waves Class (Application Class 1)
 Waves::Waves(inputQueue *InputQueue, signalQueue *SignalQueue){}
 
 Waves::~Waves(){}
 
-void Waves::setSine(){}
+void Waves::setSine()
+{
+	std::vector<double> wave;
+	uint32_t sampleRate = 200;
+	wave.clear();  // Clear previous data
+	for (uint32_t i = 0; i < sampleRate; i++)
+	{
+		// Calculate the angle in radians
+	    double angle = 2 * M_PI * frequency * static_cast<double>(i) / sampleRate;
+	    // Calculate the sine value and scale by amplitude
+	    double sample = (amplitude * std::sin(angle));
+	    wave.push_back(sample);  // Store the sample in the vector
+	 }
+}
 
 void Waves::setSquare(){}
 
