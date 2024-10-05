cd55d77bdba5db8d6c37013217e037091da6ce67
nprupp21 <>
2024-10-05 16:01:59 -0400

    Created Functions part 1

--- Code/Core/Inc/nathan.h
+++ Code/Core/Inc/nathan.h
@@ -19,7 +19,9 @@ private:
 	uint8_t pin_number_2;
 	inputValues currentInput;
 	uint8_t CurrentOutput;
-	void KnobStateMachine(uint32_t* KnobValue);
+	bool ActivatedPin1 = false;
+	bool ActivatedPin2 = false;
+	void KnobStateMachine(bool* KnobChange, bool ActivatedPin1, bool ActivatedPin2);
 public:
 	KnobDriver(GPIO_TypeDef* GpioName1, uint8_t PinNumber1, GPIO_TypeDef* GpioName2, uint8_t PinNumber2);
 	uint8_t getCurrentOutput();
@@ -33,7 +35,8 @@ private:
 	uint8_t pin_number;
 	bool currentOutput;
 	Semaphore *ButtonSemaphoreInstance;
-	void ButtonStateMachine(uint8_t* ButtonValue);
+	bool ButtonCurrentState = false;
+	void ButtonStateMachine(bool* ButtonChange, bool CurrentState);
 public:
 	ButtonDriver(GPIO_TypeDef* GpioName, uint8_t PinNumber,Semaphore *ButtonSemaphoreI);
 	bool getCurrentOutput();
@@ -47,11 +50,10 @@ private:
 	uint8_t pin_number;
 	uint8_t CurrentOutput;
 	Semaphore *SwitchSemaphoreInstance;
-	void SwitchStateMachine(bool* SwitchState);
+	bool SwitchCurrentState = false;
+	void SwitchStateMachine(bool* SwitchChange, bool CurrentState);
 public:
 	SwitchDriver(GPIO_TypeDef* GpioName, uint8_t PinNumber,Semaphore *SwitchSemaphoreI);
-	void checkForInput();
-	uint8_t getCurrentOutput();
 };
 
 class InputDriver
--- Code/Core/Src/nathan.cpp
+++ Code/Core/Src/nathan.cpp
@@ -7,8 +7,30 @@
 
 #include "allIncludes.h"
 
-KnobDriver::KnobDriver(GPIO_TypeDef* GpioName1, uint8_t PinNumber1, GPIO_TypeDef* GpioName2, uint8_t PinNumber2){}
-void KnobDriver::KnobStateMachine(uint32_t* KnobValue){
+KnobDriver::KnobDriver(GPIO_TypeDef* GpioName1, uint8_t PinNumber1, GPIO_TypeDef* GpioName2, uint8_t PinNumber2){
+
+}
+
+void KnobDriver::KnobStateMachine(bool* KnobChange, bool ActivatedPin1, bool ActivatedPin2){
+
+	static bool PreviousStatePin1 = false;
+	static bool PreviousStatePin2 = false;
+	bool CurrentStatePin1 = false;
+	bool CurrentStatePin2 = false;
+
+	if ((gpio_name_1 -> IDR & (1<<pin_number_1)) == 1) CurrentStatePin1 = true;
+	else CurrentStatePin1 = false;
+	if ((gpio_name_2 -> IDR & (1<<pin_number_2)) == 1) CurrentStatePin2 = true;
+	else CurrentStatePin2 = false;
+
+	if ((CurrentStatePin1 == true) && (PreviousStatePin1 == false)) ActivatedPin1 = true;
+	else ActivatedPin1 = false;
+	if ((CurrentStatePin2 == true) && (PreviousStatePin2 == false)) ActivatedPin2 = true;
+	else ActivatedPin2 = false;
+
+	if (ActivatedPin1 != ActivatedPin2) *KnobChange = true;
+	else *KnobChange = false;
+
 	return;
 }
 uint8_t KnobDriver::getCurrentOutput(){
@@ -24,8 +46,23 @@ void KnobDriver::checkForInput(){
 
 
 
-ButtonDriver::ButtonDriver(GPIO_TypeDef* GpioName, uint8_t PinNumber, Semaphore *ButtonSemaphoreI){}
-void ButtonDriver::ButtonStateMachine(uint8_t* ButtonValue){
+ButtonDriver::ButtonDriver(GPIO_TypeDef* GpioName, uint8_t PinNumber, Semaphore *ButtonSemaphoreI){
+
+}
+
+void ButtonDriver::ButtonStateMachine(bool* ButtonChange, bool CurrentState){
+
+	static bool ButtonPreviousState = false;
+
+	if ((gpio_name -> IDR & (1<<pin_number)) == 0) CurrentState = true;
+	else CurrentState = false;
+
+	if ((CurrentState == true) && (ButtonPreviousState == false)){
+		*ButtonChange = true;
+		ButtonPreviousState = CurrentState;
+	}
+	else *ButtonChange = false;
+
 	return;
 }
 
@@ -46,17 +83,19 @@ SwitchDriver::SwitchDriver(GPIO_TypeDef* GpioName, uint8_t PinNumber, Semaphore 
 
 }
 
-void SwitchDriver::SwitchStateMachine(bool* SwitchState){
-	return;
-}
+void SwitchDriver::SwitchStateMachine(bool* SwitchChange, bool CurrentState){
 
-uint8_t SwitchDriver::getCurrentOutput(){
-	return 0;
-}
+	static bool SwitchPreviousState = false;
 
+	if ((gpio_name -> IDR & (1<<pin_number)) == 1) CurrentState = true;
+	else CurrentState = false;
 
+	if (CurrentState != SwitchPreviousState){
+		*SwitchChange = true;
+		SwitchPreviousState = CurrentState;
+	}
+	else *SwitchChange = false;
 
-void SwitchDriver::checkForInput(){
 	return;
 }
 
