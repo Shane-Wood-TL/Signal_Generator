e36f47f0feea07f695b98347e498436b8d6a5551
nprupp21 <>
2024-10-22 18:06:47 -0400

    fixed switch driver

--- Code/Core/Src/nathan.cpp
+++ Code/Core/Src/nathan.cpp
@@ -69,11 +69,6 @@ void KnobDriver::UpdateKnob(struct inputValues *queue_data){
 	return;
 }
 
-
-
-
-
-
 ButtonDriver::ButtonDriver(GPIO_TypeDef* GpioName, uint8_t PinNumber, Semaphore *ButtonSemaphoreI){
 	ButtonSemaphoreInstance = ButtonSemaphoreI;
 	gpio_name = GpioName;
@@ -100,11 +95,6 @@ void ButtonDriver::UpdateButton(struct inputValues *queue_data){
 	return;
 }
 
-
-
-
-
-
 SwitchDriver::SwitchDriver(GPIO_TypeDef* GpioName, uint8_t PinNumber, Semaphore *SwitchSemaphoreI){
 	SwitchSemaphoreInstance = SwitchSemaphoreI;
 	gpio_name = GpioName;
@@ -115,25 +105,18 @@ void SwitchDriver::UpdateSwitch(struct inputValues *queue_data){
 
 	bool SwitchCurrentState;
 
-	if ((gpio_name -> IDR & (1<<pin_number)) == 1) SwitchCurrentState = true;
+	if ((gpio_name -> IDR & (1<<pin_number)) == 0) SwitchCurrentState = true;
 	else SwitchCurrentState = false;
 
 	if (SwitchCurrentState == true){
 		queue_data -> Switch = 1;
 	}
-	else if (SwitchCurrentState == false){
-		queue_data -> Switch = 0;
-	}
-	else {
+	else{
 		queue_data -> Switch = 0;
 	}
 
 	return;
 }
-
-
-
-
 
 InputDriver::InputDriver(KnobDriver *AmpKnobI, KnobDriver *FreqKnobI, KnobDriver *ShiftKnobI, SwitchDriver *channelSwitcherI, ButtonDriver *modeSwitcherI, inputQueue *inputQueueInstanceI, Semaphore *KnobSemaphoreI){
 	AmpKnob = AmpKnobI;
@@ -146,12 +129,15 @@ InputDriver::InputDriver(KnobDriver *AmpKnobI, KnobDriver *FreqKnobI, KnobDriver
 }
 
 void InputDriver::checkForUpdates(){
+
     inputValues queue_data = {0};
+
 	AmpKnob -> UpdateKnob(&queue_data);
     FreqKnob -> UpdateKnob(&queue_data);
     ShiftKnob -> UpdateKnob(&queue_data);
 	channelSwitcher->UpdateSwitch(&queue_data);
 	modeSwitcher -> UpdateButton(&queue_data);
+
 	inputQueueInstance -> enqueue(queue_data);
 
 
